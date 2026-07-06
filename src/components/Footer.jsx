e4bbe1afae6c8e546f490047dd5346e2014c5dfe
priyamm1007-jpg <priyamm1007@gmail.com>
2026-07-06 19:42:12 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,35 @@
+import "../styles/footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="footerTop">
+
+        <h3>ResourceHub</h3>
+
+        <p>
+          Curated websites, AI tools, coding roadmaps and resources.
+        </p>
+
+      </div>
+
+      <div className="footerBottom">
+
+        <span>
+          © 2026 ResourceHub
+        </span>
+
+        <a
+          href="https://instagram.com/bits.priyam"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Instagram
+        </a>
+
+      </div>
+
+    </footer>
+  );
+}
