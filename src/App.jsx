1258d1707945f0ec25f484c5ad8ee423aca3e54c
PRIYAM MISHRA <priyamm1007@gmail.com>
2026-07-07 12:47:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,41 @@
-import "./styles/card.css";
+import { useEffect, useState } from "react";
 import Home from "./pages/Home";
+import "./index.css";
 
 export default function App() {
-  return <Home />;
+
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+
+    const move = (e) => {
+
+      setPosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+
+    };
+
+    window.addEventListener("mousemove", move);
+
+    return () =>
+      window.removeEventListener("mousemove", move);
+
+  }, []);
+
+  return (
+    <>
+      <div
+        className="spotlight"
+        style={{
+          left: position.x,
+          top: position.y,
+        }}
+      />
+
+      <Home />
+    </>
+  );
+
 }
